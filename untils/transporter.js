16aa916cf1dc4227a079da5c02cf6db1decc934d
jaypaldhapa123 <jaypladhapa7990@gamil.com>
2025-12-15 21:20:02 +0530

    changed email file

--- untils/transporter.js
+++ untils/transporter.js
@@ -12,8 +12,6 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // must be false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // App Password
