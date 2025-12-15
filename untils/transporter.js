b159456014bcea92f093a76e0175b5c392139610
jaypaldhapa123 <jaypladhapa7990@gamil.com>
2025-12-15 20:46:38 +0530

    fixed main mail

--- untils/transporter.js
+++ untils/transporter.js
@@ -1,11 +1,26 @@
 const nodemailer = require("nodemailer");
 
- const transpotar = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+//  const transpotar = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
 
-  module.exports = transpotar;
+
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // must be false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // App Password
+  },
+});
+
+module.exports = this.transporter;
+
+
+  module.exports = transporter;
