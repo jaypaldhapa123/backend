0ea898ea9dfaeea1658e7544c7e6bc4f3ae1b9d1
jaypaldhapa123 <jaypladhapa7990@gamil.com>
2025-12-16 07:58:55 +0530

    added resend mail

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,8 @@
 const Contact = require("../models/ContactModel");
 const sendMainEmail = require("../untils/sendMainEmail");
 const sendThankYou = require("../untils/sendThankYou");
+const {Resend} = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 exports.submitForm = async (req,res) =>{
@@ -8,7 +10,7 @@ exports.submitForm = async (req,res) =>{
         const {name,email,phone,company,country,flag,message,}=req.body;
 
         //main Email
-        // await sendMainEmail(req.body);
+        await sendMainEmail(req.body);
         // const mainemail =  sendMainEmail(req.body);
 
         //AutoReply Thank you Email
@@ -17,6 +19,16 @@ exports.submitForm = async (req,res) =>{
 
         //save into database
         // await Contact.create({name,email,phone,company,country,message,});
+
+        //resend mail
+
+        const data = await resend.emails.send({
+            from:"contact  <onboarding@resend.dev>",
+            to:["contact.webzentra@gmail.com"],
+            subject:"Resnd test email",
+            html:`<h1> new message</h1>`
+        });
+
         const savedata =  await Contact.create({name,email,phone,company,country,message,});
 
         // await Promise.all([sendMainEmail,thankyoumail,savedata]);
